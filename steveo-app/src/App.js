5c71fca63d435b1949f5e796183f01beb88e0de3
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-12 10:26:45 -0600

    setting up home component

--- steveo-app/src/App.js
+++ steveo-app/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
+import Home from './components/Home';
 
 function App() {
+
+  const location = useLocation();
+
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+
   return (
-    <div className="App">
-      <h1 className="logo">LOGO HERE</h1>
-      <div className="buttons">
-        <button className="regbutton">Register</button>
-        <button className="loginbutton">Login</button>
-      </div>
+    <div>
+      {transitions.map(({ item: location, props, key }) => (
+        <animated.div key={key} style={props}>
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </animated.div>
+        ))} 
     </div>
   );
 }
---
+++ steveo-app/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1 className="logo">LOGO HERE</h1>
+      <div className="buttons">
+        <button className="regbutton">Register</button>
+        <button className="loginbutton">Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
