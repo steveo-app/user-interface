f1c3deb7c29c4cfba1d63f1ebc45de7389104014
anthonyvigz <anthonyvigz@gmail.com>
2019-11-13 23:07:45 -0500

    setting up hole functionality

--- steveo-app/src/components/HoleType.jsx
+++ steveo-app/src/components/HoleType.jsx
@@ -3,7 +3,8 @@ import AbsoluteWrapper from './AbsoluteWrapper';
 
 function HoleType(props) {
 
-    const [playerScore, setPlayerScore] = useState(0);
+    const [holeScore, setHoleScore] = useState();
+    const [playerScore, setPlayerScore] = useState();
 
     const changeHandler = event => {
         event.preventDefault();
@@ -12,12 +13,20 @@ function HoleType(props) {
             return player.name === event.target.name
           })
 
-        setPlayerScore(currentPlayer.score);
+        setHoleScore(currentPlayer.tempScore);
 
     }
 
     const addToScore = event => {
+        event.preventDefault();
 
+        const currentPlayer = props.players.filter(player => {
+            return player.name === event.target.name
+          })
+
+        setPlayerScore(currentPlayer.tempScore + currentPlayer.score)
+
+        console.log(playerScore);
     }
 
     return (
@@ -29,8 +38,9 @@ function HoleType(props) {
                     return (
                         <div className="playerAndScore">
                             <h2>{player.name}</h2>
-                            <input type="number" placeholder="strokes" name={player.name} onChange={changeHandler} value={playerScore} />
+                            <input type="number" placeholder="strokes" name={player.name} onChange={changeHandler} value={holeScore} />
                             <h3>Score: {player.score}</h3>
+                            <button name={player.name} onClick={addToScore}>ADD SCORE</button>
                         </div>
                     )
                 })}
