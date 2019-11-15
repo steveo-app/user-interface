4c4d7e0988836bd26e5ff509232672b8fb4f1d6c
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-15 15:33:34 -0600

    setting up endgame page

--- steveo-app/src/components/EndGame.jsx
+++ steveo-app/src/components/EndGame.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import AbsoluteWrapper from './AbsoluteWrapper';
 import { connect } from 'react-redux';
+import '../css/endgame.css';
 
 function EndGame(props) {
 
     const { location, players, holes } = props.game;
 
 
+    document.body.style.backgroundColor = "#4392F1";
+
+    console.log(players);
+
+    const biggest = Math.max.apply(Math, players.map(function(x) { return x.score; }))
+
+    const winner = players.filter(player => {
+        return player.score === biggest;
+    })
+
+    console.log(winner)
+
     return (
         <AbsoluteWrapper>
-            end of the game!!!
+            <div className="endGame">
+                <h1>GAME OVER</h1>
+                <h2>Congrats {winner.name}!</h2>
+                <div className="allScores">
+                    {players.map(player => {
+                        return (
+                            <div className="finalCard">
+                                <h3>{player.name}</h3>
+                                <h4>Final score:{player.score}</h4>
+                            </div>
+                        )
+                    })}
+                </div>
+
+
+            </div>
         </AbsoluteWrapper>
     )
 }
